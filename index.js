78c143ea07aac7d5c0f7814436fa039cbdacd57c
Rc515 <93041286+Rc515@users.noreply.github.com>
2025-10-30 23:21:53 +0000

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Archaeology Tracker loaded");
-  alert("Archaeology Tracker loaded successfully!");
-});
-
-document.addEventListener("click", () => {
-  alert("Click detected!");
-});
